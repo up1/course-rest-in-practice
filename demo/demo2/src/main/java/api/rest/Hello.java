948025221e3758732faccbe6ff25ff60bcb8aac8
somkiat <somkiat.p@gmail.com>
2016-01-10 18:49:48 +0700

    Add parameter in hello service

--- demo/demo2/src/main/java/api/rest/Hello.java
+++ demo/demo2/src/main/java/api/rest/Hello.java
@@ -2,22 +2,19 @@ package api.rest;
 
 import api.bean.Message;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class Hello {
 
     @GET
-    @Path("/hello")
+    @Path("/hello/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Message sayHi() {
+    public Message sayHi(@PathParam("name") String name) {
         Message helloMessage = new Message();
         helloMessage.setId(1);
-        helloMessage.setName("First time");
+        helloMessage.setName("Hello :: " + name);
         helloMessage.setDescription("Nice to meet you all");
 
         return helloMessage;
