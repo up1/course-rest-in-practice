ac6fc88fb3727e2a52fa3f1f4e54c45e20f4283b
somkiat <somkiat.p@gmail.com>
2016-01-10 19:16:26 +0700

    Re-Oganized import

--- demo/demo2/src/main/java/api/rest/MyApplication.java
+++ demo/demo2/src/main/java/api/rest/MyApplication.java
@@ -4,7 +4,6 @@ package api.rest;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
-import java.util.Map;
 
 @ApplicationPath("")
 public class MyApplication extends ResourceConfig {
